a3c9cc43824e8051e64179e8b19e4563103bb4b8
Nguyen Kien Duy <0306231185@caothang.edu.vn>
2026-01-14 22:13:56 +0700

    tao migration tat ca cac bang va them du lieu cho tags, recipes, users

---
+++ BEBepViet4.0/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $file = File::get(path: "database/json/user.json");
+        $users = collect(json_decode($file));
+        $users->each(function ($user) {
+            User::create([
+                "username" => $user->username,
+                "email" => $user->email,
+                "password" => $user->password,
+                "full_name" => $user->full_name,
+                "role" => $user->role
+            ]);
+        });
+    }
+}
