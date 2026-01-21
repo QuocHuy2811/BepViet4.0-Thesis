d21375720b6bd46d641372ebfb6e70525aad88da
Le Vu Vinh Khang <0306231205@caothang.edu.vn>
2026-01-21 20:25:16 +0700

    aaa

--- BEBepViet4.0/routes/api.php
+++ BEBepViet4.0/routes/api.php
@@ -20,6 +20,15 @@ Route::middleware('auth:sanctum')->group(function () {
             "user" => $request->user()
         ]);
     });
+    //api gợi ý món ăn bằng AI
+Route::post('/ai/suggest-recipes', [AiChefController::class, 'suggestRecipes']);
+// api load danh sách categories
+Route::get('/admin/categories', [CategoryController::class, 'index'])->name('category.index');
+// api add category
+Route::post('admin/categories/add', [CategoryController::class, 'store'])->name('category.store');
+// api update category
+Route::get('admin/categories/{id}', [CategoryController::class, 'show'])->name('category.show');
+Route::put('admin/categories/{id}', [CategoryController::class, 'update'])->name('category.update');
 });
 Route::get("/", [HomeController::class, "index"]);
 //Mguyen Kien Duy 21/01/2026 11:00
@@ -56,12 +65,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// api load danh sách categories
-Route::get('/admin/categories', [CategoryController::class, 'index'])->name('category.index');
-// api add category
-Route::post('admin/categories/add', [CategoryController::class, 'store'])->name('category.store');
-// api update category
-Route::get('admin/categories/{id}', [CategoryController::class, 'show'])->name('category.show');
-Route::put('admin/categories/{id}', [CategoryController::class, 'update'])->name('category.update');
-//api gợi ý món ăn bằng AI
-Route::post('/ai/suggest-recipes', [AiChefController::class, 'suggestRecipes']);
+
+
