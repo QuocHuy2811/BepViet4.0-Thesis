332588765d0ba33ff62b1b16a6a445b1c44a7118
QuocHuy2811 <0306231201@caothang.edu.vn>
2026-01-21 12:01:47 +0700

    Upload chuc nang dang cong thuc

--- BEBepViet4.0/routes/api.php
+++ BEBepViet4.0/routes/api.php
@@ -1,8 +1,21 @@
 <?php
-
+//
+use App\Http\Controllers\RecipeController;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route công khai (Ai cũng xem được danh mục)
+Route::get('/categories', function () {
+    return Category::select('id', 'name')->get();
+});
+
+// Route YÊU CẦU ĐĂNG NHẬP (Token hợp lệ mới vào được đây)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Di chuyển route đăng bài vào trong nhóm này
+    Route::post('/recipes', [RecipeController::class, 'store']);
+});
