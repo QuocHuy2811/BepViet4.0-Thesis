07f843ab8c6696c7845ced80f104a32626476dc8
Nguyen Kien Duy <0306231185@caothang.edu.vn>
2026-01-21 16:05:29 +0700

    Sua loi conflict

--- BEBepViet4.0/config/cors.php
+++ BEBepViet4.0/config/cors.php
@@ -15,7 +15,6 @@ return [
     |
     */
 
-<<<<<<< HEAD
     'paths' => ['api', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
@@ -32,15 +31,4 @@ return [
 
     'supports_credentials' => false,
 
-=======
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'], // Cho phép React
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
-    
->>>>>>> feature/DangCongThuc
 ];
